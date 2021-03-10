d8b9dfbe018a4d6c477d8664fb002f293fb8c2a0
Mathieu Lièvre <mathieu.lievre@efrei.net>
2021-03-10 13:48:47 +0100

    Login Screen

--- app/App.js
+++ app/App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Regform from "./components/RegForm";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Regform></Regform>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#36485f",
+    paddingLeft: 60,
+    paddingRight: 60,
   },
 });
